d5b6ef30fb24eee6945a4eb9cad45e4f1a3dab1d
schalky <steve@phase2industries.com>
2021-12-16 09:24:14 -0800

    updated build dependencies

--- lib/app/profile/components/version_info.dart
+++ lib/app/profile/components/version_info.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class VersionInfo extends StatelessWidget {
   const VersionInfo({Key? key}) : super(key: key);
