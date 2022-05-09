555acf1ef76ed6628c7c992d0cd795bd2af64858
Bo Lopker <lopkerk@gmail.com>
2022-05-08 18:19:10 -0700

    Fix headline font weight

--- lib/theme/std_app_text_styles.dart
+++ lib/theme/std_app_text_styles.dart
@@ -7,7 +7,7 @@ class StdAppTextStyles extends AppTextStyles {
           headline1: TextStyle(
               fontFamily: 'Recoleta',
               fontSize: 32,
-              fontWeight: FontWeight.w700,
+              fontWeight: FontWeight.w600,
               height: 43.52 / 32,
               color: themeColors.secondaryText),
           body: TextStyle(
