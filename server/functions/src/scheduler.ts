12237129093681cb0681c638cf1e133925af0271
Steve Ansell <ansell@phase2industries.com>
2022-10-04 18:42:13 -0700

    Expose endpoint to run scheduler in live env
    Created independent function  to run the scheduled tasks
    Re-ordered tasks to make sure we don't mark expired and then end in a single run
    Created HTTP endpoint to directly run the tasks
    Removed local emulator endpoint to run scheduler once since it was redundant
    Refactored the local simulation of the PubSub scheduler since functions can only run for a max of 9 minutes so it now runs once and publishes a message on a timeout to re-run itself

--- server/functions/src/scheduler.ts
+++ server/functions/src/scheduler.ts
@@ -9,10 +9,26 @@ import {PubSub} from "@google-cloud/pubsub";
 const expiringMinutes = 5;
 
 export const scheduler = functions.pubsub.schedule("every 5 minutes").onRun(async () => {
-  await setExpiringSessions();
-  await endExpiredSessions();
+  await performScheduledTasks();
   return null;
 });
+
+export const runScheduler = functions.https.onRequest(async (_, response) => {
+  await performScheduledTasks();
+  response.status(200).send("Schedule has run");
+});
+
+/**
+ * Run all of the currently scheduled tasks. We can later break this up into separate sections
+ * if we end up needing to run tasks at different intervals or want to run specific tasks on demand
+ * @return {Promise}
+ */
+async function performScheduledTasks(): Promise<void> {
+  // Do them in this order so we don't end sessions that were just set to expiring in the same run
+  // This gives the frontend a chance to handle the expiring state
+  await endExpiredSessions();
+  await setExpiringSessions();
+}
 
 /**
  * Queries snapCircles for any sessions that have expired and ends them
@@ -55,31 +71,40 @@ if (process.env.FUNCTIONS_EMULATOR === "true") {
   // These endpoints are only available in the local emulator because it doesn't support actually schduling
   // the pubsub functions https://github.com/firebase/firebase-tools/issues/2034#issuecomment-1215972419
 
+  const SCHEDULER_FUNCTION_TOPIC = "firebase-schedule-scheduler";
+  const PUBSUB_SIMULATE_TOPIC = "firebase-pubsub-simulate";
   /**
    * Runs the scheduler by publishing a message to the scheduler topic
+   * @param {string} topic - The topic to publish to
+   * @return {Promise}
    */
-  const _publishToScheduler = async () => {
+  const _publishToTopic = async (topic: string): Promise<void> => {
     const pubsub = new PubSub({projectId: process.env.GCLOUD_PROJECT});
     try {
-      const SCHEDULED_FUNCTION_TOPIC = "firebase-schedule-scheduler";
-      console.log(`Trigger sheduled function via PubSub topic: ${SCHEDULED_FUNCTION_TOPIC}`);
+      console.log(`Trigger sheduled function via PubSub topic: ${topic}`);
       const data = Buffer.from("start");
-      const msg = await pubsub.topic(SCHEDULED_FUNCTION_TOPIC).publishMessage({data});
+      const msg = await pubsub.topic(topic).publishMessage({data});
       console.log(`Message ${msg} published.`);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // Hit this endpoint to manually publish a message to the sceduler and run it immediately
-  exports.runScheduler = functions.https.onRequest((req, response) => {
-    _publishToScheduler();
-    response.status(200).send("Published message to scheduler");
-  });
+  // Pubsub handler with a timeout of 6 minutes so that it set a 5 minute timeout to send a message to run the scheduler
+  // and then send a message to itself to run again
+  exports.pubSubScheduler = functions
+    .runWith({timeoutSeconds: 60 * 6})
+    .pubsub.topic(PUBSUB_SIMULATE_TOPIC)
+    .onPublish(async () => {
+      setTimeout(async () => {
+        await _publishToTopic(SCHEDULER_FUNCTION_TOPIC);
+        await _publishToTopic(PUBSUB_SIMULATE_TOPIC);
+      }, 5 * 60 * 1000);
+    });
 
-  // Hit this endpoint to pubish a message to the scheduler every 5 minutes
-  exports.startScheduler = functions.https.onRequest((req, response) => {
-    setInterval(async () => _publishToScheduler, 5 * 60 * 1000); // every 5 minutes
+  // Hit this endpoint to start simulating the pubsub scheduler running every 5 minutes
+  exports.simulateScheduler = functions.https.onRequest(async (_, response) => {
+    await _publishToTopic(PUBSUB_SIMULATE_TOPIC);
     response.status(200).send("Scheduler started to run every 5 minutes");
   });
 }
