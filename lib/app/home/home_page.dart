39549fac1435afb59863412f4ce85faba8847665
schalky <steve@phase2industries.com>
2022-10-31 11:50:36 -0700

    Fix #378
    Updated home page to use sliver list to combine lists into a single scrollable view.
    
    Updated gradle for android which seemed to help fix a build issue.

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -6,14 +6,10 @@ import 'package:totem/app/home/components/index.dart';
 import 'package:totem/app/home/components/named_circle_list.dart';
 import 'package:totem/components/widgets/index.dart';
 import 'package:totem/models/index.dart';
+import 'package:totem/services/index.dart';
 import 'package:totem/theme/index.dart';
-import '../../services/providers.dart';
-import 'menu.dart';
 
-final myPrivateCircles = StreamProvider.autoDispose<List<SnapCircle>>((ref) {
-  final repo = ref.read(repositoryProvider);
-  return repo.mySnapCircles();
-});
+import 'menu.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -67,53 +63,52 @@ class HomePage extends ConsumerWidget {
   Widget _homeContent(BuildContext context, WidgetRef ref,
       {required bool isMobile, required AuthUser user}) {
     final t = AppLocalizations.of(context)!;
-    bool isKeeper = user.hasRole(Role.keeper);
-    bool hasPrivateCircles = false;
-    List<SnapCircle> privateWaitingCircles = [];
-    return ref.watch(myPrivateCircles).when(
-          data: (List<SnapCircle> data) {
-            if (data.isNotEmpty) {
-              hasPrivateCircles = true;
-              privateWaitingCircles = data
-                  .where((element) => element.state == SessionState.waiting)
-                  .toList();
-            }
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(
-                  height: 60,
-                ),
-                TotemHeader(
-                  text: t.circles,
-                  trailing: !isMobile && (isKeeper || !hasPrivateCircles)
-                      ? const CreateCircleButton()
-                      : null,
-                ),
-                SizedBox(height: isMobile ? 30 : 20),
-                if (isMobile)
-                  Padding(
-                    padding: EdgeInsets.only(
-                        bottom: 20,
-                        left: Theme.of(context).pageHorizontalPadding,
-                        right: Theme.of(context).pageHorizontalPadding),
-                    child: Row(children: const [CreateCircleButton()]),
-                  ),
-                if (privateWaitingCircles.isNotEmpty)
-                  NamedCircleList(
-                      name: t.yourPrivateCircles,
-                      circles: privateWaitingCircles),
-                const SnapCirclesRejoinable(),
-                const Expanded(
-                  child: SnapCirclesList(
-                    topPadding: 15,
-                  ),
-                ),
-              ],
-            );
-          },
-          loading: () => Container(),
-          error: (Object error, StackTrace? stackTrace) => Container(),
-        );
+    final bool isKeeper = user.hasRole(Role.keeper);
+    final bool hasPrivateCircles =
+        ref.watch(userPrivateCircles).value?.isNotEmpty ?? false;
+    final bool hasRejoinable =
+        ref.watch(rejoinableCircles).value?.isNotEmpty ?? false;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const SizedBox(
+          height: 60,
+        ),
+        TotemHeader(
+          text: t.circles,
+          trailing: (isKeeper || !hasPrivateCircles)
+              ? const CreateCircleButton()
+              : null,
+        ),
+        Expanded(
+          child: CustomScrollView(
+            slivers: [
+              NamedCircleList(
+                name: t.yourPrivateCircles,
+              ),
+              const SnapCirclesRejoinable(),
+              SliverToBoxAdapter(
+                child: (hasPrivateCircles || hasRejoinable)
+                    ? Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 8.0,
+                            horizontal:
+                                Theme.of(context).pageHorizontalPadding),
+                        child: Text(
+                          t.otherCircles,
+                          style: Theme.of(context).textStyles.headline2,
+                        ),
+                      )
+                    : const SizedBox(height: 10),
+              ),
+              const SnapCirclesList(),
+              const SliverToBoxAdapter(
+                child: SizedBox(height: 20),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
