cb705efa4c2d217f3129b9b4902419849c2fa273
Bo Lopker <lopkerk@gmail.com>
2022-11-26 15:16:18 -0800

    Metaballs and less icons in wait animation

--- lib/components/animation/wait_animation.dart
+++ lib/components/animation/wait_animation.dart
@@ -2,7 +2,6 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:lucide_icons/lucide_icons.dart';
 
 import 'draw_arc.dart';
 
