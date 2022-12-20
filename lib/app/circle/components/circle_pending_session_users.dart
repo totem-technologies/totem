1a88e8685560b956d42cff274ab93bfb5a1fe372
Bo Lopker <lopkerk@gmail.com>
2022-12-20 15:46:24 -0800

    Fix padding on waiting room

--- lib/app/circle/components/circle_pending_session_users.dart
+++ lib/app/circle/components/circle_pending_session_users.dart
@@ -5,16 +5,20 @@ import 'package:totem/app/circle/components/circle_session_participant.dart';
 
 import 'circle_network_connectivity_layer.dart';
 import 'layouts.dart';
+import 'package:totem/theme/index.dart';
 
 class CirclePendingSessionUsers extends ConsumerWidget {
   const CirclePendingSessionUsers({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
     final session = ref.watch(activeSessionProvider);
     final participants = session.activeParticipants;
     return CircleNetworkConnectivityLayer(
       child: Padding(
-        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+        padding: EdgeInsets.only(
+            left: theme.pageHorizontalPadding,
+            right: theme.pageHorizontalPadding),
         child: Center(
           child: WaitingRoomListLayout(
               count: participants.length,
