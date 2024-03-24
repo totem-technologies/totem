7775c99b08a926f71ea0de07a20f2c99c6ee66b8
Bo Lopker <lopkerk@gmail.com>
2024-03-24 13:09:21 -0700

    Got android building

--- app/lib/services/audio_level/audio_level_mobile.dart
+++ app/lib/services/audio_level/audio_level_mobile.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
-import 'audio_level_impl.dart';
 
 import 'package:noise_meter/noise_meter.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+import 'audio_level_impl.dart';
 
 class AudioLevel extends AudioLevelImpl {
   StreamSubscription<NoiseReading>? _noiseSubscription;
@@ -11,8 +12,8 @@ class AudioLevel extends AudioLevelImpl {
   @override
   void start() async {
     await Permission.microphone.request();
-    _noiseMeter = NoiseMeter((error) {});
-    _noiseSubscription = _noiseMeter!.noiseStream.listen((event) {
+    _noiseMeter = NoiseMeter();
+    _noiseSubscription = _noiseMeter!.noise.listen((event) {
       emitData(event.meanDecibel);
     });
   }
