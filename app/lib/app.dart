a4b88b2ed884e532dc9aebbcff961a943255cd4e
Bo Lopker <lopkerk@gmail.com>
2024-03-24 15:08:58 -0700

    Move theme

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,4 +1,3 @@
-import 'package:cupertino_will_pop_scope/cupertino_will_pop_scope.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -57,76 +56,7 @@ class _AppState extends ConsumerState<App> {
       ],
       debugShowCheckedModeBanner: false,
       title: 'Totem',
-      theme: _appTheme(context),
+      theme: totemTheme(),
     ));
-  }
-
-  ThemeData _appTheme(BuildContext context) {
-    AppThemeColors themeColors = StdAppThemeColors();
-    AppTextStyles textStyles = StdAppTextStyles(themeColors);
-    AppThemeStyles.setStyles(colors: themeColors, textStyles: textStyles);
-    return ThemeData(
-      appBarTheme: AppBarTheme(
-        centerTitle: true,
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-        iconTheme: IconThemeData(color: themeColors.primaryText),
-      ),
-      colorScheme: ColorScheme.fromSwatch(
-        accentColor: themeColors.primary, // Overscroll color on Android
-      ),
-      primaryColor: themeColors.primary,
-      scaffoldBackgroundColor: themeColors.screenBackground,
-      fontFamily: 'Montserrat',
-      dialogTheme: DialogTheme(
-        backgroundColor: themeColors.dialogBackground,
-        contentTextStyle: textStyles.dialogContent,
-      ),
-      textTheme: textStyles,
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-            foregroundColor: themeColors.linkText,
-            textStyle: textStyles.textLinkButton),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        labelStyle: textStyles.inputLabel,
-        hintStyle: textStyles.hintInputLabel,
-        errorStyle: textStyles.bodyLarge!
-            .copyWith(color: themeColors.error, fontSize: 12),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(
-            color: themeColors.divider,
-            width: 1.0,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(
-            color: themeColors.divider,
-            width: 1.0,
-          ),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(
-            color: themeColors.error,
-            width: 1.0,
-          ),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(
-            color: themeColors.divider,
-            width: 1.0,
-          ),
-        ),
-      ),
-      pageTransitionsTheme: const PageTransitionsTheme(
-        builders: {
-          TargetPlatform.android: ZoomPageTransitionsBuilder(),
-          TargetPlatform.iOS: CupertinoWillPopScopePageTransionsBuilder(),
-        },
-      ),
-    );
   }
 }
--- app/lib/theme/index.dart
+++ app/lib/theme/index.dart
@@ -1,5 +1,80 @@
+import 'package:cupertino_will_pop_scope/cupertino_will_pop_scope.dart';
+import 'package:flutter/material.dart';
+
+import 'app_text_styles.dart';
+import 'app_theme_colors.dart';
+import 'app_theme_styles.dart';
+import 'std_app_text_styles.dart';
+import 'std_app_theme_colors.dart';
+
 export 'app_text_styles.dart';
 export 'app_theme_colors.dart';
 export 'app_theme_styles.dart';
-export 'std_app_text_styles.dart';
-export 'std_app_theme_colors.dart';
+
+ThemeData totemTheme() {
+  AppThemeColors themeColors = StdAppThemeColors();
+  AppTextStyles textStyles = StdAppTextStyles(themeColors);
+  AppThemeStyles.setStyles(colors: themeColors, textStyles: textStyles);
+  return ThemeData(
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      iconTheme: IconThemeData(color: themeColors.primaryText),
+    ),
+    colorScheme: ColorScheme.fromSwatch(
+      accentColor: themeColors.primary, // Overscroll color on Android
+    ),
+    primaryColor: themeColors.primary,
+    scaffoldBackgroundColor: themeColors.screenBackground,
+    fontFamily: 'Montserrat',
+    dialogTheme: DialogTheme(
+      backgroundColor: themeColors.dialogBackground,
+      contentTextStyle: textStyles.dialogContent,
+    ),
+    textTheme: textStyles,
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+          foregroundColor: themeColors.linkText,
+          textStyle: textStyles.textLinkButton),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: textStyles.inputLabel,
+      hintStyle: textStyles.hintInputLabel,
+      errorStyle: textStyles.bodyLarge!
+          .copyWith(color: themeColors.error, fontSize: 12),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: themeColors.divider,
+          width: 1.0,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: themeColors.divider,
+          width: 1.0,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: themeColors.error,
+          width: 1.0,
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: themeColors.divider,
+          width: 1.0,
+        ),
+      ),
+    ),
+    pageTransitionsTheme: const PageTransitionsTheme(
+      builders: {
+        TargetPlatform.android: ZoomPageTransitionsBuilder(),
+        TargetPlatform.iOS: CupertinoWillPopScopePageTransionsBuilder(),
+      },
+    ),
+  );
+}
