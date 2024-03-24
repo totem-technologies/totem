38f4459c6bbccb97cbd279adaadf9fc442989df9
Bo Lopker <lopkerk@gmail.com>
2024-03-24 16:51:17 -0700

    Fix body text

--- app/lib/theme/std_app_text_styles.dart
+++ app/lib/theme/std_app_text_styles.dart
@@ -5,86 +5,74 @@ class StdAppTextStyles extends AppTextStyles {
   StdAppTextStyles(AppThemeColors themeColors)
       : super(
           headline1: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 32,
               fontWeight: FontWeight.w600,
               height: 43.52 / 32,
               color: themeColors.primaryText),
-          body: TextStyle(
-              fontFamily: 'Montserrat',
+          bodyLarge: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.w600,
               color: themeColors.primaryText),
+          bodyMedium: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+              color: themeColors.primaryText),
           button: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 16,
               fontWeight: FontWeight.w600,
               color: themeColors.primaryText),
           pinInput: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 20,
               fontWeight: FontWeight.w600,
               color: themeColors.primaryText),
           headline2: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 24,
               fontWeight: FontWeight.w400,
               height: 1.5,
               color: themeColors.secondaryText),
           headline6: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 24,
               fontWeight: FontWeight.w600,
               color: themeColors.primaryText),
           headline3: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 16,
               height: 18.78 / 16,
               fontWeight: FontWeight.w600,
               color: themeColors.primaryText),
           headline4: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 14,
               height: 18 / 14,
               fontWeight: FontWeight.w500,
               color: themeColors.secondaryText),
           headline5: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 12,
               fontWeight: FontWeight.w600,
               color: themeColors.reversedText),
           inputLabel: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 16,
               fontWeight: FontWeight.bold,
               color: themeColors.primaryText),
           formErrorLabel: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 12,
               fontWeight: FontWeight.normal,
               color: themeColors.error),
           textLinkButton: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 16,
               fontWeight: FontWeight.w600,
               color: themeColors.linkText),
           dialogContent: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 16,
               color: themeColors.primaryText,
               fontWeight: FontWeight.normal),
           dialogTitle: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 24,
               fontWeight: FontWeight.w600,
               color: themeColors.primaryText),
           hintInputLabel: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 14,
               color: themeColors.secondaryText,
               fontWeight: FontWeight.w500),
           nextTag: TextStyle(
-              fontFamily: 'Montserrat',
               fontSize: 14,
               height: 1.5,
               fontWeight: FontWeight.w500,
